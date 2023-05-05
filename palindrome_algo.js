212232f19fcee37322f9b0cb354a6a7d1feb09d1
Marvis Knight <knightmar78@gmail.com>
2023-05-05 08:38:21 -0700

    group algos, carlie, david and gary

---
+++ palindrome_algo.js
@@ -0,0 +1,52 @@
+/* 
+  String: Is Palindrome
+  Create a function that returns a boolean whether the string is a strict palindrome. 
+    - palindrome = string that is same forwards and backwards
+  
+  Do not ignore spaces, punctuation or capitalization
+*/
+// RIOT Read Input Output Talk
+const str1 = "a x a";
+const expected1 = true;
+
+const str2 = "racecar";
+const expected2 = true;
+
+const str3 = "Dud";
+const expected3 = false;
+
+const str4 = "oho!";
+const expected4 = false;
+
+const str5 = "abba"
+const expected5 = true;
+
+
+function isPalindrome(str) {
+    const len = str.length;
+    for (var i = 0; i < str.length/2; i++ ){
+        if (str[i] !== str[len - 1 -i]){
+            return false
+        }
+    }
+    return true
+}
+
+
+function isPalindrome(str) {
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  var len = str.length;
+  for (var i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return str;
+    }
+  }
+  return str;
+}
+
+console.log(isPalindrome(str1)) //expected: true
+console.log(isPalindrome(str2)) //expected: true
+console.log(isPalindrome(str3)) //expected: false
+console.log(isPalindrome(str4)) //expected: false
+console.log(isPalindrome(str5)) //expected: true
